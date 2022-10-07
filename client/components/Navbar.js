78798cee5e45d603f4c33af06e328ac5482ef827
Gagan Kadadevarmath <gagan2@illinois.edu>
2022-10-07 17:44:43 -0500

    Implementing basic header

---
+++ client/components/Navbar.js
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from 'react';
+import MenuIcon from '@mui/icons-material/Menu';
+import { AppBar, Box, Button, ClickAwayListener, Container, IconButton, Menu, MenuItem, Toolbar, Typography, Avatar } from '@mui/material';
+import Link from 'next/link';
+import { useStyles } from './NavbarStyles.js';
+
+export default function Navbar() {
+    const classes = useStyles();
+    const logo = '/assets/raf_logo.png'
+    const logo_words = '/assets/raf_logo_words.png'
+
+    const [needsMenu, setNeedsMenu] = useState(null);
+
+    const handleOpenMenu = (event) => {
+        setNeedsMenu(event.currentTarget);
+    };
+
+    const handleCloseMenu = () => {
+        setNeedsMenu(null);
+    };
+
+    const [currentPage, setCurrentPage] = useState("")
+
+    useEffect(() => {
+        var curPage = window.location.href.substring(window.location.href.lastIndexOf('/') + 1);
+        setCurrentPage(curPage);
+    }, [])
+
+    return (
+        <>
+            <AppBar className={classes.header}>
+                <Container maxWidth="xl">
+                    <Toolbar disableGutters>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            onClick={handleOpenMenu}
+                            color="info"   
+                            className={classes.sidebarOpener}
+
+                        >
+                            <MenuIcon />
+                        </IconButton>
+
+                        <Box className={classes.logoBox}>
+                            <Link href="/" passHref>
+                                <a rel="noreferrer">
+                                    <img src={logo} alt='logo_symbol' className={classes.logo} />
+                                </a>
+                            </Link>
+                            <Link href="/" passHref>
+                                <a rel="noreferrer">
+                                    <img src={logo_words} alt='logo' className={classes.wordsLogo} />
+                                </a>
+                            </Link>
+                        </Box>
+
+                        <Box className={classes.util}>
+                            <Box className={classes.tools}>
+                                <a rel="noreferrer">
+                                    <img src="/assets/search.png" alt='search' />
+                                </a>
+                                <a rel="noreferrer">
+                                    <img src="/assets/new.png" alt='notifications' />
+                                </a>
+                            </Box>
+                            <Typography sx={{ color: '#DFE0EB', fontSize: "30px", textAlign: 'center', padding: '0 15px' }}>|</Typography>
+                            <Box className={classes.profile}>
+                                <Typography sx={{ color: 'black', textAlign: 'center' }}>FirstName LastName</Typography>
+                                <Avatar>OP</Avatar>
+                            </Box>
+                        </Box>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </>
+    )
+}
---
+++ client/components/NavbarStyles.js
@@ -0,0 +1,224 @@
+import { makeStyles } from '@mui/styles';
+
+const tablet = '850px';
+const mobile = '420px';
+
+const useStyles = makeStyles({
+    header: {
+        position: 'fixed',
+        width: '100%',
+        zIndex: 5,
+
+        display: 'flex',
+        padding: '2rem',
+        background: 'white',
+        boxShadow: 'none',
+        top: 0,
+        left: 0,
+        // border: 'red 3px solid'
+    },
+
+    sidebarOpener: {
+        padding: 0,
+        marginRight: '30px',
+        position: 'relative',
+        left: 0,
+    },
+
+    logoBox: {
+        position: 'realtive',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'flex-start',
+    },
+
+    wordsLogo: {
+        position: 'static',
+        paddingTop: '15px',
+    
+
+        [`@media (max-width: ${mobile})`]: {
+            display: 'none'
+        },
+
+        flex: 'none',
+        order: 0,
+        flexGrow: 0,
+        // margin: '0px 10px',
+    },
+
+    logo: {
+        position: 'static',
+        width: '77px',
+        height: '78px',
+        left: '0px',
+        top: '0px',
+
+        [`@media (max-width: ${mobile})`]: {
+            width: '60px',
+            height: '60px',
+        },
+
+        flex: 'none',
+        order: 0,
+        flexGrow: 0,
+        margin: '0px 10px',
+    },
+
+    pages: {
+        display: 'flex',
+
+        flexGrow: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingLeft: '10rem',
+
+        ['@media (max-width: 1000px)']: {
+            display: 'none',
+        }
+    },
+
+    page: {
+        textDecoration: 'none',
+        cursor: 'pointer',
+        color: 'black',
+        marginLeft: '38px',
+        fontFamily: 'Roboto',
+        fontStyle: 'normal',
+        fontWeight: 700,
+        fontSize: '16px',
+
+        ['@media (max-width: 1000px)']: {
+            display: 'none',
+        },
+
+        '&:hover, &:visited, &:focus, &:active': {
+            textDecoration: 'none',
+            borderBottom: "3px solid",
+
+            ['@media (max-width: 1000px)']: {
+                display: 'none',
+            }
+        }
+    },
+
+    activePage: {
+        textDecoration: 'none',
+        borderBottom: "3px solid",
+        cursor: 'pointer',
+        color: 'white',
+        marginLeft: '38px',
+        fontFamily: 'Roboto',
+        fontStyle: 'normal',
+        fontWeight: 700,
+        fontSize: '16px',
+        
+        '&:hover, &:visited, &:focus, &:active': {
+            textDecoration: 'none',
+            borderBottom: "3px solid",
+            cursor: 'pointer',
+            color: 'white',
+            marginLeft: '38px',
+            fontFamily: 'Roboto',
+            fontStyle: 'normal',
+            fontWeight: 700,
+            fontSize: '16px',
+
+            ['@media (max-width: 1000px)']: {
+                display: 'none',
+            }
+        }
+    },
+
+    navButtons: {
+        display: 'flex',
+        alignItems: 'center',
+        flexGrow: 0,
+
+        ['@media (max-width: 1000px)']: {
+            display: 'none',
+        }
+    },
+
+    navButton: {
+        textTransform: 'none',
+        fontFamily: 'Roboto',
+        fontStyle: 'normal',
+        fontWeight: 70,
+        fontSize: '16px',
+
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 'none',
+        order: 1,
+        flexGrow: 0,
+        margin: '0px 20px',
+        padding: '12px 30px',
+        borderRadius: '30px',
+
+        width: '126px',
+        height: '60px',
+
+        whiteSpace: 'nowrap',
+        textAlign: 'center',
+
+        ['@media (max-width: 1000px)']: {
+            display: 'none',
+        }
+    },
+
+    menuBox: {
+        display: 'none',
+        ['@media (max-width: 1000px)']: {
+            display: 'flex',
+            position: 'absolute',
+            flexGrow: 1,
+            right: 0,
+        }
+    },
+
+    menu: {
+        display: 'none',
+        ['@media (max-width: 1000px)']: {
+            display: 'flex',
+            position: 'absolute',
+            right: 0,
+        }
+    },
+
+    floatCon: {
+        float: 'right'
+    },
+
+    util: {
+        width: 'auto',
+        position: 'absolute',
+        display: 'flex',
+        flexDirection: 'row',
+        right: 0,
+        justifyContent: 'center',
+        alignItems: 'center',
+        // float: 'right'
+        // border: 'red 3px solid'
+    },
+
+    tools: {
+        display: 'flex',
+        flexDirection: "row",
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: '15px'
+    },
+
+    profile: {
+        display: 'flex',
+        flexDirection: "row",
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: '15px'
+    }
+}, { index: 1 });
+
+export { useStyles };
