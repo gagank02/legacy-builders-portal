eb076d4824492012d9abe8deea2e216aa94d536e
02danieljonas <02danieljonas@gmail.com>
2022-10-08 06:08:28 -0400

    comments table can be updated and queried

--- api/models/comments.js
+++ api/models/comments.js
@@ -1,17 +1,98 @@
-const db = require("../db")
+const db = require("../db");
+const { BadRequestError } = require("../utils/errors");
 
 class Comment {
-    static async getComment(commentID) {
-        return await db.query('SELECT * FROM Comments WHERE ID = ($1)',[commentID]);
+    static async getAllCommentOnPage(body) {
+        const requiredFields = ["pageID"];
+
+        console.log(body);
+
+        requiredFields.forEach((field) => {
+            if (!body.hasOwnProperty(field)) {
+                throw new BadRequestError(`Missing ${field} in request body.`);
+            }
+        });
+
+        let res = await db.query(
+            "SELECT page_id, user_id, comment, parent_comment_id, id FROM Comments WHERE page_id = $1",
+            [body.pageID]
+        );
+        if (res.rows[0] == undefined) {
+            throw new BadRequestError(`Comment not found.`);
+        }
+
+        const comments = [];
+
+        res.rows.forEach(async (e) =>
+            comments.push(await Comment.makePublicComment(e))
+        );
+        return comments;
     }
 
-    static async setComment({page_idValue, user_idValue, commentValue, parent_comment_idValue}) {
-        if (parent_comment_idValue == null) {
-            return await db.query("INSERT INTO comments(page_id, user_id, comment) VALUES ($1, $2, $3) RETURNING page_id, user_id, comment, parent_comment_id, id;",[page_idValue, user_idValue, commentValue]);
+    static async getComment(body) {
+        const requiredFields = ["commentID"];
+
+        requiredFields.forEach((field) => {
+            if (!body.hasOwnProperty(field)) {
+                throw new BadRequestError(`Missing ${field} in request body.`);
+            }
+        });
+
+        const { commentID } = body;
+
+        console.log(commentID);
+
+        let res = await db.query(
+            "SELECT page_id, user_id, comment, parent_comment_id, id FROM Comments WHERE ID = ($1)",
+            [commentID]
+        );
+        if (res.rows[0] == undefined) {
+            throw new BadRequestError(`Comment not found.`);
         }
-        else {
-            return await db.query("INSERT INTO comments(page_id, user_id, comment, parent_comment_id) VALUES ($1, $2, $3, $4) RETURNING page_id, user_id, comment, parent_comment_id, id;",[page_idValue, user_idValue, commentValue, parent_comment_idValue]);
-        }
+        return Comment.makePublicComment(res.rows[0]);
+    }
+
+    static async makePublicComment(comment) {
+        console.log(comment);
+        return {
+            commentID: comment.id,
+            comment: comment.comment,
+            pageID: comment.page_id,
+            userID: comment.user_id,
+            parentCommentID: comment.parent_comment_id,
+        };
+    }
+
+    static async setComment(res, values) {
+        const requiredFields = ["page_idValue", "commentValue"];
+
+        requiredFields.forEach((field) => {
+            if (!values.hasOwnProperty(field)) {
+                throw new BadRequestError(`Missing ${field} in request body.`);
+            }
+        });
+
+        let comment;
+
+        // if (values.parent_comment_idValue == undefined) {
+        comment = await db.query(
+            "INSERT INTO comments(page_id, comment, user_id) VALUES ($1, $2, (select id from users where email = $3)) RETURNING page_id, user_id, comment, parent_comment_id, id;",
+            [values.page_idValue, values.commentValue, res.locals.user.email]
+        );
+        // }
+        // else {
+        //     comment = await db.query(
+        //         "INSERT INTO comments(page_id, comment, parent_comment_id, user_id) VALUES ($1, $2, $3, (select id from users where email = $4)) RETURNING page_id, user_id, comment, parent_comment_id, id;",
+        //         [
+        //             values.page_idValue,
+        //             values.commentValue,
+        //             1,
+        //             res.locals.user.email,
+        //         ]
+        //     );
+        // }
+
+        return Comment.makePublicComment(comment.rows[0]);
     }
 }
 
--- api/routes/contents.js
+++ api/routes/contents.js
@@ -1,48 +1,78 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Courses = require('../models/courses');
-const Quizzes = require('../models/quizzes');
+const Courses = require("../models/courses");
+const Quizzes = require("../models/quizzes");
 // const UserCourses = require('../models/user-courses');
-const Pages = require('../models/pages');
-const Comments = require('../models/comments');
+const Pages = require("../models/pages");
+const Comments = require("../models/comments");
 // const QuizResults = require('../models/quiz-results');
-const { validateToken } = require("../utils/tokens");
+const security = require("./../middleware/security");
 
-
-
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
     // console.log(req.body);
-    res.send('hello world')
+    res.send("hello world");
     // console.log("test");
-})
+});
 
-router.get('/courses', async (req, res) => {
-    req.decoded = validateToken(req.body.token);
-    var course = await Courses.getCourse(req.body);
-    console.log(course);   
-    return res.status(200).json({token, course});
-})
+router.get("/courses", async (req, res) => {
+    try {
+        req.decoded = validateToken(req.body.token);
+        var course = await Courses.getCourse(req.body);
+        console.log(course);
+        return res.status(200).json({ token, course });
+    } catch (error) {
+        next(error);
+    }
+});
 
-router.get('/quizzes', async (req, res) => {
-    req.decoded = validateToken(req.body.token);
-    var quiz = await Quizzes.getQuiz(req.body);
-    console.log(quiz);    
-    return res.status(200).json({token, quiz});
-})
+router.get("/quizzes", security.requireAuthenticatedUser, async (req, res) => {
+    try {
+        var quiz = await Quizzes.getQuiz(req.body);
+        console.log(quiz);
+        return res.status(200).json({ token, quiz });
+    } catch (error) {
+        next(error);
+    }
+});
 
-router.get('/pages', async (req, res) => {
-    req.decoded = validateToken(req.body.token);
+router.get("/pages", security.requireAuthenticatedUser, async (req, res) => {
     var page = await Pages.getPage(req.body);
-    console.log(page);     
-    return res.status(200).json({token, page});
-})
+    console.log(page);
+    return res.status(200).json({ token, page });
+});
 
-router.get('/comments', async (req, res) => {
-    req.decoded = validateToken(req.body.token);
-    var comment = await Comments.getComment(req.body);
-    console.log(comment);
-    res.send('hello world');   
-})
+router.post("/comments/commentsonpage", async (req, res, next) => {
+    try {
+        let comments = await Comments.getAllCommentOnPage(req.body);
+        console.log(comments);
+        return res.status(201).send(comments);
+    } catch (error) {
+        next(error);
+    }
+});
 
+router.post("/comments/get", async (req, res, next) => {
+    try {
+        let comment = await Comments.getComment(req.body);
+        console.log(comment);
+        return res.status(201).send(comment);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.post(
+    "/comments/set",
+    security.requireAuthenticatedUser,
+    async (req, res, next) => {
+        try {
+            var comment = await Comments.setComment(res, req.body);
+            console.log(comment.rows);
+            return res.status(201).send(comment);
+        } catch (error) {
+            next(error);
+        }
+    }
+);
 
 module.exports = router;
