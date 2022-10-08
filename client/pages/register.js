f8ee0480e403cfd754f13b48475ba0c08eb37f36
Gagan Kadadevarmath <gagan2@illinois.edu>
2022-10-08 01:42:44 -0500

    register

--- client/pages/register.js
+++ client/pages/register.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import MuiSubmitButton from "../components/MuiSubmitButton";
 import MuiSignin from "../components/MuiSignin";
+import axios from 'axios';
 
 const API_URL = 'http://localhost:3001'
 
