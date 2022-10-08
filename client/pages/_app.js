55055a0c1d6531af0051c2e25572b11c50c8fe38
mattwhyte <whytema1@msu.edu>
2022-10-07 22:57:50 -0500

    Communit Post card made

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -3,6 +3,7 @@ import '../components/Sidebar.css'
 import '../components/index.css'
 import '../components/profile.css'
 import '../components/signIn.css'
+import '../components/community.css'
 
 function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />
--- client/pages/community.js
+++ client/pages/community.js
@@ -3,13 +3,37 @@ import Head from 'next/head'
 import Navbar from '../components/Navbar'
 
 
+const CommunityPost = ({header, firstName, lastName}) => {
+  return ( 
+      <>
+      <div className='CommunityPost'>
+        <div>
+          <img className="image" src="/assets/raf_logo.png"></img>
+        </div>
+        <div className='info'>
+          <div className='header'>
+            <h1>{header}</h1>
+          </div>
+          <div className='bottom'>
+            <h4>By: {firstName} {lastName}</h4>
+            <h4 className='comments'>Comments</h4>
+          </div>
+        </div>
+      </div>
+      </>
+   );
+}
 
 export default function Community() {
   return (
       <>
       <div>
         <Navbar></Navbar>
-        
+        <CommunityPost header='Blockchain developers best practices on innovation chain' firstName='first' lastName='last'></CommunityPost>
+        <br></br>
+        <CommunityPost header='OnePay - Online Payment Processing App' firstName='another' lastName='name'></CommunityPost>
+        <br></br>
+        <CommunityPost header='Blockchain developers best practices on innovation chain' firstName='something' lastName='else'></CommunityPost>
       </div>
       </>
   );
