30ba25097b0e568c46707b5d48b8f9ad472e0888
mattwhyte <whytema1@msu.edu>
2022-10-08 04:01:48 -0500

    Information on cards and location based

--- client/pages/resources.js
+++ client/pages/resources.js
@@ -4,7 +4,9 @@ import MuiResourceCard from '../components/MuiResourceCard';
 const Resources = () => {
     const volunteers = [
         {firstName: 'Jane', lastName:'Doe', location:'Chicago, Illinois', description: "Jane is a volunteer from Chicago, Illinois who is an expert in financial planning. She is available to meet in person or online. Reach out at janedoe@gmail.com!"},
-        {firstName: 'Tom', lastName:'Smith', location:'Naperville, Illinois', description: "Top is an expert in project management and emotional intelligence. He is available to help on the weekends. He can be reached at tomsmith@gmail.com!"}
+        {firstName: 'Tom', lastName:'Smith', location:'Naperville, Illinois', description: "Tom is an expert in project management and emotional intelligence. He is available to help on the weekends. He can be reached at tomsmith@gmail.com!"},
+        {firstName: 'Benjamin', lastName:'Chase', location:'Evanston, Illinois', description: "Ben is a volunteer and considered very knowledgable in leadership and soft skills. He is available weeknights. Reach out at benchase@gmail.com!"},
+        {firstName: 'John', lastName:'Morgan', location:'Chicago, Illinois', description: "John is a professional in financial literacy and conflict resolution. He is available anytime at jpmorgan@gmail.com!"},
     ]
 
     return ( 
@@ -20,11 +22,10 @@ const Resources = () => {
             </div>
             
             <br></br>
-
             <div className='cards'>
-                <MuiResourceCard></MuiResourceCard>
+                <MuiResourceCard firstName={volunteers[2].firstName} lastName={volunteers[2].lastName} location={volunteers[2].location} description={volunteers[2].description}></MuiResourceCard>
                 <br></br>
-                <MuiResourceCard></MuiResourceCard>
+                <MuiResourceCard firstName={volunteers[3].firstName} lastName={volunteers[3].lastName} location={volunteers[3].location} description={volunteers[3].description}></MuiResourceCard>
             </div>
 
         </div>
