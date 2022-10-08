afb3dbac5f4646c2eac9a044529cf5548f6b6df8
mattwhyte <whytema1@msu.edu>
2022-10-08 00:21:40 -0500

    Initial community post commit

---
+++ client/pages/communityPost.js
@@ -0,0 +1,18 @@
+import Navbar from '../components/Navbar'
+
+const CommunityPost = ({title}) => {
+    return ( 
+        <>
+        <div>
+          <Navbar></Navbar>
+          <h1>Create a new post!</h1>
+          <form>
+            
+          </form>
+
+        </div>
+        </>
+     );
+}
+ 
+export default CommunityPost;
