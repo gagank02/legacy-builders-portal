aeab86134da4f476d3cf42fec87609ef4c641467
Gagan Kadadevarmath <gagan2@illinois.edu>
2022-10-08 01:38:55 -0500

    register

--- client/pages/register.js
+++ client/pages/register.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 import MuiSubmitButton from "../components/MuiSubmitButton";
 import MuiSignin from "../components/MuiSignin";
 
+const API_URL = 'http://localhost:3001'
+
 const Register = () => {
     
     const [userName, setUserName] = useState('');
@@ -12,16 +14,32 @@ const Register = () => {
     
     const handleSubmit = (e) => {
         e.preventDefault();
-        const user = {userName, password};
+        // const usdater = {userName, password};
 
-        fetch('', {
-            method: 'POST',
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(user)
-        }).then(() => {
-            console.log("User signed in");
+        // fetch('', {
+        //     method: 'POST',
+        //     headers: {"Content-Type": "application/json"},
+        //     body: JSON.stringify(user)
+        // }).then(() => {
+        //     console.log("User signed in");
 
-        });
+        // });
+
+        const data = {
+            userName: userName,
+            password: password,
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+        }
+
+        axios.post(`${API_URL}/auth/sign-up`, data)
+            .then(function(res) {
+                console.log(res);
+            })
+            .catch(function(err) {
+                console.log(err)
+            });
     }
 
 
@@ -49,17 +67,17 @@ const Register = () => {
                     <br></br>
                     <div className="input-container">
                         <label>First Name: </label>
-                        <input type="first name" name="pass" value={firstName} required onChange={(e) => setPassword(e.target.value)}/>
+                        <input type="first name" name="pass" value={firstName} required onChange={(e) => setFirstName(e.target.value)}/>
                     </div>
                     <br></br>
                     <div className="input-container">
                         <label>Last Name: </label>
-                        <input type="last name" name="pass" value={lastName} required onChange={(e) => setPassword(e.target.value)}/>
+                        <input type="last name" name="pass" value={lastName} required onChange={(e) => setLastName(e.target.value)}/>
                     </div>
                     <br></br>
                     <div className="input-container">
                         <label>Email: </label>
-                        <input type="email" name="pass" value={email} required onChange={(e) => setPassword(e.target.value)}/>
+                        <input type="email" name="pass" value={email} required onChange={(e) => setEmail(e.target.value)}/>
                     </div>
                     <br></br>
                     </form>
